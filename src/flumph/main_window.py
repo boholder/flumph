2d6526f6577d01e02675e5c45ed015c2fec1aedf
boholder <bottleholder@anche.no>
2023-07-22 17:09:00 +0800

    Add pyside6

--- src/flumph/main_window.py
+++ src/flumph/main_window.py
@@ -1,2 +1,14 @@
+import sys
+
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QLabel
+
+
 def start_main_window():
-    pass
+    app = QApplication(sys.argv)
+
+    label = QLabel("Hello World", alignment=Qt.AlignCenter)
+
+    label.show()
+
+    sys.exit(app.exec_())
